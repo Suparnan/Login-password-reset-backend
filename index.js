1587342acc0a7a0752712319200cd4220d177a95
Suparnan <79783697+Suparnan@users.noreply.github.com>
2021-08-11 12:41:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import express from "express";
-import mongoose from "mongoose";
-import {User} from "./models/users.js"
-import cors from "cors";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require('cookie-parser')
 
 const app = express();
-const PORT = 4180;
+const PORT = process.env.PORT || 4180;
+require("dotenv").config();
 
+
+//DB connection
 const url="mongodb+srv://Suparnan:Guvi@123@cluster0.clkv3.mongodb.net/userAuth";
 //const url="mongodb://localhost/userAuth";
 mongoose.connect(url,{useNewUrlParser: true,
@@ -14,29 +17,19 @@ mongoose.connect(url,{useNewUrlParser: true,
 const con = mongoose.connection;
 con.on('open',() => console.log("Database connected succesfully"));
 
+//Middleware
 app.use(express.json());
 app.use(cors());
 
+//API Title
 app.get('/', (request, response) => {
-    // console.log('Hello There, your GET message is successful')
-    // const msge = "your GET message is successful";
-    response
-    // .status('Hello There, We are in this localhost '+PORT,' and your GET message is successful')
-    .send("your GET message is successful");
+   response.json({message:"API created"});
 });
 
-app.get('/users', async (request, response) => {
-    const uname = await User.find({});
-    response.send(uname);
-});
+//Router linking
+app.use("/auth", require("./routes/auth.routes.js"));
 
-app.get('/use/:email/:pwd',cors(), async (request, response) => {
-    const {email} = request.params;
-    const {pwd} = request.params;
-    const uname = await User.find({email:{$eq:email}},{_id:0,username:0});
-    response.send(uname);
-});
-
+//Server Connection
 app.listen(PORT,() => {
     console.log("The express server started successfully in the",+PORT);
 })
