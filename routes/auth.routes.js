da155232cc6ec4e501536934926c7e70fc2ea366
Suparnan <79783697+Suparnan@users.noreply.github.com>
2021-08-11 12:39:45 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,38 +1,8 @@
-import express from 'express';
-import { userSchema } from "./models/users.js"
+const router = require("express").Router();
+const {userController} = require("../controller/user.controller.js");
 
-const bcrypt = require('bcrypt');
-const router = express.Router();
-
-router.post("/signin-user", (req,res,next) => {
-    let getUser;
-    userSchema.findOne({email: req.body.email})
-    .then(user => {
-        if(!user) {
-            return res.status(401).json({message:'Auth Failed'});
-        }
-        getUser = user;
-        return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(response => {
-        if(!response) {
-            return res.status(401).json({message:'Auth Failed'});
-        }
-        let jwtToken = jwt.sign({
-            email: getUser.email,
-        }, "longer secret is better",{
-            expiry: "2h"
-        });
-        res.status(200).json({
-            token: jswToken,
-            expiresIn : 6600,
-            msg: getUser
-        })    
-        .catch((err) => {
-            return res.status(401).json({message:'Auth Failed'});
-        });
-    });
-});
-
+router.post("/signup", userController.createSignup);
+router.post("/login", userController.login);
+router.post("/reset", userController.reset);
 
 module.exports = router;
