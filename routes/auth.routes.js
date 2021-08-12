663f0ac5b95d44cd1d75ed87ef7a87770e29c766
Suparnan <79783697+Suparnan@users.noreply.github.com>
2021-08-12 15:11:35 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,5 +4,7 @@ const {userController} = require("../controller/user.controller.js");
 router.post("/signup", userController.createSignup);
 router.post("/login", userController.login);
 router.post("/reset", userController.reset);
+router.post("/resetval", userController.resetval);
+router.post("/resetform", userController.resetform);
 
 module.exports = router;
