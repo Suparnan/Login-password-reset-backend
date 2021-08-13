7df8ec6c55be77bdbf4e14d9a0716936734b7185
Suparnan <79783697+Suparnan@users.noreply.github.com>
2021-08-13 16:21:31 +0530

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,8 +2,7 @@ const User = require("../models/users.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
-const cryptoRandomString = require("crypto-random-string");
-const { async } = require("crypto-random-string");
+const crypto = require("crypto");
 
 const transport = nodemailer.createTransport({
     host: "in-v3.mailjet.com",
@@ -15,131 +14,74 @@ const transport = nodemailer.createTransport({
 });
 
 const userController = {
-   
+
     // createSignup will validate and post the userData in to the Database
     createSignup: async (request, response) => {
-        try{
-            const {username, email, password, confirmpassword} = request.body;
+        try {
+            const { username, email, password, confirmpassword } = request.body;
 
-            if(!username || !email || !password || !confirmpassword){
-                return response.status(400).json({message:"All the fields are required"});
-             }
+            if (!username || !email || !password || !confirmpassword) {
+                return response.status(400).json({ message: "All the fields are required" });
+            }
 
-            const user = await User.findOne({email})
-            if(user) return response.status(400).json({message:"User already exist"});
+            const user = await User.findOne({ email })
+            if (user) return response.status(400).json({ message: "User already exist" });
 
             const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if(!re.test(email))
-                return response.status(400).json({message:"Incorrect email"});
+            if (!re.test(email))
+                return response.status(400).json({ message: "Incorrect email" });
 
-            if(password.length < 6) return response.status(400).json({message:"password should be more than 4 charecters"});
+            if (password.length < 6) return response.status(400).json({ message: "password should be more than 4 charecters" });
 
-            if(password != confirmpassword)
-                return response.status(403).json({message:"Password doesn't match, Please enter the password properly"})
-             
+            if (password != confirmpassword)
+                return response.status(403).json({ message: "Password doesn't match, Please enter the password properly" })
 
-            const hashpass =  await bcrypt.hash(password, 10);
+
+            const hashpass = await bcrypt.hash(password, 10);
             const saveUser = new User({
-                username ,
-                email ,
+                username,
+                email,
                 password: hashpass,
             })
 
             await saveUser.save()
             return response.status(200).json(saveUser)
-           
+
         } catch (error) {
-            return response.status(404).json({messge:error.message});
+            return response.status(404).json({ messge: error.message });
         }
     },
 
     login: async (request, response) => {
-        try{
+        try {
             const { email, password } = request.body;
-            console.log("inside Login",email,password);
+            console.log("inside Login", email, password);
 
-            if( !email || !password) {
-                return response.status(400).json({message:"All the fields are required"});
-            }            
-            
-            const user = await User.findOne({email});
-            if(!user) return response.status(400).json({message:"Email doesn't exist"});
-            
+            if (!email || !password) {
+                return response.status(400).json({ message: "All the fields are required" });
+            }
+
+            const user = await User.findOne({ email });
+            if (!user) return response.status(400).json({ message: "Email doesn't exist" });
+
             const doMatch = await bcrypt.compare(password, user.password);
-            if(!doMatch) return response.status(400).json({message:"Password Incorrect"});
-            
-            // if(doMatch){ 
-            //     const token = jwt.sign({_id:user._id}, "enamoragasiyam", {expiresIn:"1d"});
-            //     const {_id, username, email} = user
+            if (!doMatch) return response.status(400).json({ message: "Password Incorrect" });
 
-            //     transport.sendMail({
-            //         to: email,
-            //         from: 'sonuragavan27@gmail.com',
-            //         subject: `Signup Successful`,
-            //         html:`
-            //         <h1>Welcome, ${user.username}</h1>
-            //         <h5>Sign-up Successful</h5>
-            //         `,
-            //     });
-
-
-            //     console.log('outside sendmail')
-               
-            //     response.status(200).json({
-            //         mytoken: token,
-            //         user: {
-            //             _id,
-            //             username,
-            //             email,
-            //         }
-            //     })
-            // }
-           
-          //  return response.status(200).json({message:"Logged-In Successfully"})
-
-        } catch (error) {
-            return response.status(400).json({messge:error.message});
-        }
-    },
-    
-    reset: async(request, response) => {
-        try{
-            const { email } = request.body;
-            const randomstr = cryptoRandomString({length: 6, type: 'distinguishable'});
-            console.log(randomstr);
-            
-            if(!email)
-            return response.status(400).json({message:"Please enter your Email"});
-
-            const user = await User.findOne({email});
-            if(!user) 
-            return response.status(400).json({message:"Please enter Registered email"});
-            
-            if(user) {
-                const token = jwt.sign({_id:user._id}, "enamoragasiyam", {expiresIn:"1d"});
-                const options = {
-                    expires: new Date(Date.now() + 900000),
-                    httpOnly: false
-                }
-
-                response.cookie('jwt-token',token,options);
+            if (doMatch) {
+                const token = jwt.sign({ _id: user._id }, "enamoragasiyam", { expiresIn: "1d" });
+                const { _id, username, email } = user
 
                 transport.sendMail({
-                    to: user.email,
+                    to: email,
                     from: 'sonuragavan27@gmail.com',
                     subject: `Signup Successful`,
-                    html:`
+                    html: `
                     <h1>Welcome, ${user.username}</h1>
-                    <p>Please click on the given link and enter the Secret Key</p>
-                    <a href="http://localhost/auth/resetval"></a>
-                    <h5>You're Secret Key is ${randomstr}</h5>
+                    <h5>Login Successful</h5>
                     `,
                 });
 
-                user.secretkey = randomstr;
-
-                console.log('outside sendmail')
-                const {_id, username, email} = user
+                console.log('outside sendmail');
                 response.status(200).json({
                     mytoken: token,
                     user: {
@@ -149,67 +91,72 @@ const userController = {
                     }
                 })
             }
-            return response.status(200).json({message:"Password reset link will be sent to your email"});    
-            
-        } catch(error) {
-            return response.status(400).json({message:error.message})
-        }
-    },
 
-    resetval: async(request, response) => {
-        try{
-            const { secretkey } = request.body;
+            return response.status(200).json({ message: "Logged-In Successfully" })
 
-            if(!secretkey){
-                return response.status(400).json({message:"Please enter secret key sent to your email"});
-            }
-
-            const match = await User.findOne({secretkey});
-            if(!match){
-                return response.status(400).json({message:"secret key does not match, Please enter valid secret key"});
-            }
-
-            if(match){
-                return response.status(200).json({messge:"secret key matches"});
-            }
-
-        } catch(error){
-            return response.status(400).json({message:error.message});
-        }
-    },
-
-    resetform: async(request, response) => {
-        try{
-            const { username, password, confirmpassword} = request.body;
-
-            if(!username || !password || !confirmpassword){
-                return response.status(400).json({message:"All the fields are required"});
-             }
-
-            const user = await User.findOne({username})
-            if(!user) return response.status(400).json({message:"Please enter appropriate username"});
-            
-            if(password.length < 6) return response.status(400).json({message:"password should be more than 4 charecters"});
-
-            if(password != confirmpassword)
-                return response.status(403).json({message:"Password doesn't match, Please enter the password properly"})
-             
-            if(user){ 
-            const hashpass =  await bcrypt.hash(password, 10);
-            const saveUser = new User({
-                password: hashpass,
-            })
-
-            await saveUser.save()
-            return response.status(200).json(saveUser)
-        }
-           
         } catch (error) {
-            return response.status(404).json({messge:error.message});
+            return response.status(400).json({ messge: error.message });
         }
     },
 
+    reset: async (request, response) => {
 
+        const { email } = request.body;
+        crypto.randomBytes(32, (err, buffer) => {
+            if (err) {
+                console.log(err);
+            }
+            const token = buffer.toString('hex');
+            User.findOne({ email: email })
+                .then(async (user) => {
+                    if (!user) {
+                        return response.status(400).json({ message: "Please enter Registered email" });
+                    }
+                    user.resetToken = token;
+                    user.expireTime = Date.now + 3600000;
+                    user.save()
+                        .then((result) => {
+                            transport.sendMail({
+                                to: user.email,
+                                from: 'sonuragavan27@gmail.com',
+                                subject: `Signup Successful`,
+                                html: `
+                            <h1>Welcome, ${user.username}</h1>
+                            <p>Please click on the given <a href="http://localhost/auth/resetform">here </a>and enter the pass code</p>
+                            <h5>You're Passcode is ${token}</h5>
+                            `,
+                            });
+                            response.json({ message: "Check your email" });
+                        })
+                })
+        })
+    },
+
+    resetform: async (request, response) => {
+
+        const { sentToken, password, confirmpassword } = request.body;
+
+        if (!sentToken || !password || !confirmpassword) {
+            return response.status(400).json({ message: "All the fields are required" });
+        }
+
+        User.findOne({ resetToken: sentToken }, { expireTime: { $gt: Date.now() } })
+            .then(user => {
+                if (!user || password != confirmpassword) {
+                    return response.status(422).json({ message: "Please try again" });
+                }
+                bcypt.hash(password, 12)
+                    .then(hashpass => {
+                        user.password = hashpass
+                        user.resetToken = undefined
+                        user.expireTime = undefined
+                        user.save()
+                            .then(() => {
+                                return response.status(200).json({ message: "Password updated successfully" })
+                            })
+                    })
+            })
+    }
 }
 
-module.exports = {userController};
+module.exports = { userController };
